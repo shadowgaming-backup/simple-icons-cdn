101820d3493390f78c538277496a4dce0f7eb2d0
shadowgaming-backup <170877225+shadowgaming-backup@users.noreply.github.com>
2024-08-23 23:11:33 +0100

    Add files via upload

--- source/app.js
+++ source/app.js
@@ -1,49 +1,25 @@
-import { getIconSvg, getSimpleIcon } from "./icon.js";
-import { Application } from "jsr:@oak/oak/application";
-import { Router } from "jsr:@oak/oak/router";
+import {getSimpleIcon, getIconSvg} from './icon.js';
 
-const router = new Router();
+const app = (request, response) => {
+	response.setHeader(
+		'Cache-Control',
+		'public, max-age=86400, s-maxage=31536000, stale-while-revalidate=604800',
+	);
 
-router.get("/", (ctx) => {
-  ctx.response.headers.set(
-    "Cache-Control",
-    "public, max-age=86400, s-maxage=31536000, stale-while-revalidate=604800",
-  );
-  ctx.response.status = 307;
-  ctx.response.redirect("https://github.com/LitoMore/simple-icons-cdn");
-});
-router.get("/favicon.ico", (ctx) => {
-  ctx.response.headers.set(
-    "Cache-Control",
-    "public, max-age=31536000, s-maxage=31536000, immutable",
-  );
-  ctx.response.status = 204;
-});
-router.get("/:iconSlug/:color?/:darkModeColor?", (ctx) => {
-  ctx.response.headers.set("Access-Control-Allow-Origin", "*");
-  ctx.response.headers.set(
-    "Cache-Control",
-    "public, max-age=86400, s-maxage=31536000, stale-while-revalidate=604800",
-  );
+	if (request.method !== 'GET') {
+		return response.status(404).send({status: 404});
+	}
 
-  const { iconSlug, color, darkModeColor } = ctx.params;
-  const viewbox = ctx.request.url.searchParams.get("viewbox");
-  const icon = getSimpleIcon(iconSlug);
+	const {iconSlug, color, darkModeColor, viewbox} = request.query;
+	const icon = getSimpleIcon(iconSlug);
 
-  if (icon) {
-    const iconSvg = getIconSvg(icon, color, darkModeColor, viewbox);
-    ctx.response.headers.set("Content-Type", "image/svg+xml");
-    ctx.response.body = iconSvg;
-    return;
-  }
+	if (icon) {
+		const iconSvg = getIconSvg(icon, color, darkModeColor, viewbox);
+		response.setHeader('Content-Type', 'image/svg+xml');
+		return response.send(iconSvg);
+	}
 
-  ctx.response.status = 404;
-  return;
-});
+	return response.status(404).send({status: 404});
+};
 
-const app = new Application();
-app.use(router.routes());
-app.use(router.allowedMethods());
-app.listen({ port: 8080 });
-
-console.log("Server running at http://localhost:8080/simpleicons");
+export default app;
